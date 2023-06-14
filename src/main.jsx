c4c2ab1f445f43642d398099e20a9556d5daf2ba
Ruveyda <ruveyday20@gmail.com>
2023-06-14 20:40:35 +0300

    feat: vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom';
+import { inject } from '@vercel/analytics';
 import App from './App.jsx'
 import './index.css';
+ 
+inject();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
